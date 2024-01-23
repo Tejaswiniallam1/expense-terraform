37bba8e57e61a0f9ec543128c9f39fd490a735b8
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-23 08:53:00 +0530

    TF Code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,6 +6,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-${var.project_name}-igw"
+  }
+}
+
 
 resource "aws_subnet" "public" {
   count             = length(var.public_subnets_cidr)
@@ -14,7 +22,21 @@ resource "aws_subnet" "public" {
   availability_zone = element(var.az, count.index)
 
   tags = {
-    Name = "public-subnet-${count.index+1}"
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-rt-${count.index + 1}"
   }
 }
 
@@ -25,7 +47,7 @@ resource "aws_subnet" "private" {
   availability_zone = element(var.az, count.index)
 
   tags = {
-    Name = "private-subnet-${count.index+1}"
+    Name = "private-subnet-${count.index + 1}"
   }
 }
 
