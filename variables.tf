ef45926148170126013899cc2abade09e3717afe
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-25 08:57:38 +0530

    TF Code

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "env" {}
 variable "project_name" {}
 variable "kms_key_id" {}
+variable "bastion_cidrs" {}
 
 variable "vpc_cidr" {}
 variable "public_subnets_cidr" {}
@@ -16,7 +17,10 @@ variable "rds_instance_class" {}
 variable "rds_family" {}
 
 variable "backend_app_port" {}
-variable "bastion_cidrs" {}
 variable "backend_instance_capacity" {}
 variable "backend_instance_type" {}
+
+variable "frontend_app_port" {}
+variable "frontend_instance_capacity" {}
+variable "frontend_instance_type" {}
 
