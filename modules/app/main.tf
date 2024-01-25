821e9d99665c6a02aa4c1b5dd0596ed184066893
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-25 08:52:29 +0530

    TF Code

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -41,7 +41,6 @@ resource "aws_launch_template" "main" {
 
 resource "aws_autoscaling_group" "main" {
   name                = "${local.name}-asg"
-  availability_zones  = ["us-east-1a"]
   desired_capacity    = var.instance_capacity
   max_size            = var.instance_capacity # TBD, THis we will fine tune after autoscaling
   min_size            = var.instance_capacity
