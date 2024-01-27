9f90a2a38f988e49b6f90daf3bf6aefe30ca18c3
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-27 09:03:45 +0530

    TF Code

--- main.tf
+++ main.tf
@@ -79,6 +79,7 @@ module "backend" {
   vpc_id              = module.vpc.vpc_id
   vpc_zone_identifier = module.vpc.app_subnets_ids
   parameters          = ["arn:aws:ssm:us-east-1:739561048503:parameter/${var.env}.${var.project_name}.rds.*"]
+  kms                 = var.kms_key_id
 }
 
 module "frontend" {
@@ -95,6 +96,7 @@ module "frontend" {
   vpc_id              = module.vpc.vpc_id
   vpc_zone_identifier = module.vpc.web_subnets_ids
   parameters          = []
+  kms                 = var.kms_key_id
 }
 
 module "public-alb" {
