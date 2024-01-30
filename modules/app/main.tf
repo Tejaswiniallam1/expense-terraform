f8078b98e362c9ed7a9921be887d71f2d266331f
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-30 08:42:20 +0530

    TF Code

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -87,6 +87,12 @@ resource "aws_autoscaling_group" "main" {
     propagate_at_launch = true
   }
 
+  tag {
+    key                 = "Monitor"
+    value               = "yes"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_lb_target_group" "main" {
