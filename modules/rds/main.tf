256bd3f9bc122ac17ad8d231abf45fc22fbc8faa
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2024-02-24 08:09:57 +0530

    Fixing Checkov DB snapshots

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -53,6 +53,7 @@ resource "aws_db_instance" "main" {
   kms_key_id             = var.kms_key_id
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
+  copy_tags_to_snapshot  = true
 }
 
 
